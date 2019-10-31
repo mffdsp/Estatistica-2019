ae4ea5965c2b1d43c9090fe4247c7fa7632b598b
Mateus Felismino <45128866+mffdsp@users.noreply.github.com>
2019-10-30 22:14:45 -0300

    Add files via upload

---
+++ main.R
@@ -0,0 +1,75 @@
+library(formattable)
+library(tidyverse)
+
+INICIAR()
+
+INICIAR = function(){
+  
+  df <- data.frame(
+    
+    #NOME
+    NOME = df0$NOME, 
+    
+    #IDADE
+    IDADE = df0$IDADE,
+    
+    #Formação
+    ESCOLARIDADE = df0$ESCOLARIDADE,
+    
+    #PESO
+    PESO = df0$PESO,
+    
+    #ALTURA
+    ALTURA = df0$ALTURA,
+    
+    stringsAsFactors = FALSE)
+  
+  setTable()
+  
+  calcularIMC(df)
+  
+  formattable(df, list(
+    IMC = formatter("span", 
+                    style = ~ style(color = ifelse(IMC >= 18.5 & IMC < 24.99, "green", "red"))),
+    ESCOLARIDADE = formatter("span", style = x ~ ifelse(x == "A", 
+                                                        style(color = "green", font.weight = "bold"), NA)),
+    area(col = c(PESO)) ~ normalize_bar("pink", 0.2),
+    area(col = c(ALTURA)) ~ normalize_bar("yellow", 0.5)
+  ))
+  
+}
+
+setTable = function(){
+  
+  df0 <- read.table("input.csv", header=TRUE, sep=",")
+  
+}
+
+
+calcularIMC = function(df){
+  df["IMC"] <- NULL
+  for(i in 1:nrow(df)) {
+    df$IMC[i] <- as.numeric(formatC((df$PESO[i] / (df$ALTURA[i])^2),  digits=3))
+    
+  }
+  df["Media"] <- NULL
+}
+
+df1 <- data.frame(
+  
+  MEDIA = formatC(mean(df$IMC), digits = 3),
+  
+  MEDIANA = formatC(median(df$IMC), digits = 3),
+  
+  DECIL4 = formatC(quantile(df$IMC, prob = seq(0, 1, length = 11), type = 5)[4], digits = 3),
+  
+  DESVIO_PADRAO = formatC(sd(df$IMC), digits = 3), 
+  
+  stringsAsFactors = FALSE)
+
+formattable(df1)
+calcularMedia = function(){
+  
+}
+
+
